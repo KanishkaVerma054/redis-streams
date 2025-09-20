cb61cfe2b4664c892ba66a047b10aa7d65cf5d33
Kanishka Verma <kanishkav774@gmail.com>
2025-09-20 14:05:42 +0530

    basics for better-uptime

--- producer/index.ts
+++ producer/index.ts
@@ -1,16 +1,20 @@
+// mongoose
 import { createClient } from "redis";
+import {prismaClient} from "store";
 
 async function main() {
+    let websites: {url: string, id: string}[] = prismaClient.website.find();
+
     const client = await createClient()
         .on("error", (err) => console.log("Redis Client Error", err))
         .connect();
-
-    const res = await client.xAdd('betteruptime:websites', '*', {
-        url: "https://streeem.com",
-        id: "1"
-    });
+    
+    const res = await client.xAdd('betteruptime:websites', '*', websites.map(website => ({
+        url: website.url,
+        id: website.id
+    })))
     console.log(res);
     client.destroy()
 }
 
-main()
+setInterval(main, 3 * 60 * 1000)
--- worker/index.ts
+++ worker/index.ts
@@ -1,24 +1,40 @@
-import { createClient } from "redis";
+import {createClient} from "redis";
 
 async function main() {
-    const client = await createClient()
+    while(1) {
+        const client = await createClient()
         .on("error", (err) => console.log("Redis Client Error", err))
         .connect();
 
-    const res = await client.xReadGroup('india', 'india-1', {
-        key: 'betteruptime:websites',
-        id: '>'
-    }, {
-        COUNT: 2
-    });
+        const res = await client.xReadGroup('usa', 'us-1', {
+            key: 'betteruptime:websites',
+            id: '>'
+        }, {
+            COUNT: 2
+        })
 
-    if(!res) {
-        client.destroy();
-        return;
+        if (!res) {
+            return;
+        }
+
+        let websitesToTrack = res[0].messages;
+        websitesToTrack.forEach(website => {
+            let startTime = Date.now()
+            axios.get(website.url)
+                .then(() => {
+                    prismaClient.WebsiteTick.create({
+                        status: "GOOD",
+                        response_time_ms: Date.now() - startTime,
+                        region_id: "india",
+                        website_id: website.id
+                    })
+                })
+                .catch(() => {
+
+                })
+        })
+
     }
-    // @ts-ignore
-    console.log(res[0].messages);
-    client.destroy();
 }
 
-main()
+main();
